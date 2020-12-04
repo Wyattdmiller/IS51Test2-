fa442fe85ee52b11bc454248ba9d043ad50764ef
Wyattdmiller <Wyatt@MacBook-Pro.local>
2020-12-03 16:22:35 -0800

    Structured English

---
+++ test2.py
@@ -0,0 +1,10 @@
+""" Structured English
+This program will open the file Final.txt, which contains student grades on a final exam.
+It will then create a list, where each item of the list is a line of the file.
+It will display the number of items in the list, which is the number of grades.
+It will then sum the grades in the list, and divide the sum by the number of grades to get an average.
+It will display the average.
+Then it will check each grade in the list and compare it to the average. 
+It will keep track of how many grades were above the average,
+and display that number as a percentage of the number of grades. 
+"""
